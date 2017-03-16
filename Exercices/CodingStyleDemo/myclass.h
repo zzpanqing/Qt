f22687e533887b32e804e5d2714234e7e982aaf2
PAN Qing <zzpanqing.learning@gmail.com>
2017-03-17 00:00:18 +0100

    CodingStyle

--- Exercices/CodingStyleDemo/myclass.h
+++ Exercices/CodingStyleDemo/myclass.h
@@ -1,11 +1,16 @@
-#ifndef MYCLASS_H
-#define MYCLASS_H
+#ifndef CODINGSTYLEDEMO_MYCLASS_H_ // #define guard 格式 ： <PROJECT>_<PATH>_<FILE>_H_
+#define CODINGSTYLEDEMO_MYCLASS_H_
 
+// header 应该其是自给自足的，code client 不需要设置任何条件就可以使用，
+// code client 也不需 include  header 所使用的 .h 或者定义 header
+// 所使用的 symbol
 
+// 尽可能地避免使用 forward declaration
+// 用前置声明代替 includes 甚至都会暗暗地改变代码的含义：
 class MyClass
 {
 public:
     MyClass();
 };
 
-#endif // MYCLASS_H
+#endif // CODINGSTYLEDEMO_MYCLASS_H_
